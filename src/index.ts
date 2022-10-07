d7ce718be7d066f7ebc1cfce4ee3b25db094cbd7
GrosserBruder <falenangellll@yandex.ru>
2022-10-07 16:37:08 +0700

    fix

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./constants"
+export * from "./hooks"
+export { default as CrudRefBook } from "./CrudRefBook"
+export type { CrudRefBookFormProps, CrudRefBookProps } from "./CrudRefBook"
